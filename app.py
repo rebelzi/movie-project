640c6a7c1464791574330561d1cf206212c01758
LAPTOP-KCAUDTIJ\FIRDAUS <firdauschuzaeni@gmail.com>
2022-12-08 14:32:02 +0700

    satu

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ def movie_post():
     star_receive = request.form['star_give']
     comment_receive = request.form['comment_give']
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive ,headers=headers)
-
+    data = requests.get(url_receive,headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
     og_image = soup.select_one('meta[property="og:image"]')
     og_title = soup.select_one('meta[property="og:title"]')
@@ -38,18 +37,19 @@ def movie_post():
     title = og_title['content']
     desc = og_description['content']
     doc = {
-        'image' : image,
-        'title' : title,
-        'description' : desc,
-        'star' : star_receive,
-        'comment' : comment_receive,
+        'image':image,
+        'title':title,
+        'description':desc,
+        'star':star_receive,
+        'comment':comment_receive
     }
     db.movies.insert_one(doc)
-    return jsonify({'msg':'POST request!'})
+    return jsonify({'msg':'GET request!'})
 
 @app.route("/movie", methods=["GET"])
 def movie_get():
-    return jsonify({'msg':'GET request!'})
+    movie_list = list(db.movies.find({},{'_id':False}))
+    return jsonify({'movies':movie_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
